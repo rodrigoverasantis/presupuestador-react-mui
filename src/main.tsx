37479a34e61c01a4b5e9f2be4f44c27dfe7396e0
rodrigoverasantis <rodrigoverasantis@gmail.com>
2025-11-29 13:42:01 -0300

    feat: Se incluye funcionalidad para usar selector de fechas

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import App from "./App.tsx";
+import "dayjs/locale/es";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
+      <App />
+    </LocalizationProvider>
   </StrictMode>,
 );
