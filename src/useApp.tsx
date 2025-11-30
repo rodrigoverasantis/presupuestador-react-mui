4f3d50bc21d77f280d8c2807034d0536bca06c50
rodrigoverasantis <rodrigoverasantis@gmail.com>
2025-11-30 12:16:42 -0300

    feat :Actualización para incluir datos del form.

--- src/useApp.tsx
+++ src/useApp.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
-import type { Item } from "./interfaces";
+import { type FormInterface, type ItemInterface } from "./interfaces";
+import { Form } from "./classes";
 
 export default function UseApp() {
-  const [items, setItems] = useState<Item[]>([]);
+  const [form, setForm] = useState<FormInterface>(new Form());
+  const [items, setItems] = useState<ItemInterface[]>([]);
 
   /**
    * Método encargado de agregar nuevos elementos.
    */
   const handleAgregar = () => {
-    let newItem: Item = {
+    let newItem: ItemInterface = {
       name: "",
       quantity: 0,
       price: 0,
@@ -31,12 +33,14 @@ export default function UseApp() {
    * @param itemIndex Índice del elemento.
    * @param editedItem Elemento con los datos actualizados.
    */
-  const handleEditar = (itemIndex: number, editedItem: Item) => {
+  const handleEditar = (itemIndex: number, editedItem: ItemInterface) => {
     const editedItems = items.map((item, index) => index === itemIndex ? editedItem : item);
     setItems(editedItems);
   }
 
   return {
+    form,
+    setForm,
     items,
     handleAgregar,
     handleEliminar,
