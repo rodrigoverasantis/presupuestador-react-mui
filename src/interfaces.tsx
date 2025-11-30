d7510938c7c8ebf1af7ff8b37b3449ff78a87e20
rodrigoverasantis <rodrigoverasantis@gmail.com>
2025-11-30 00:51:21 -0300

    feat: Actualización para incluir interface del formulario.

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -1,8 +1,17 @@
-export interface ItemsProps {
-  items: Array<Item>;
-  handleAgregar: () => void;
-  handleEliminar: (itemIndex: number) => void;
-  handleEditar: (itemIndex: number, editedItem: Item) => void;
+export interface Form {
+  cliente: string;
+  empresa: string;
+  rut: string;
+  direccion: string;
+  telefono: string;
+  email: string;
+  ciudad: string;
+  iva: number;
+  moneda: null;
+  descuento: number;
+  fechaEmision: Date;
+  fechaVencimiento: Date;
+  descripcion: string;
 }
 
 export interface Item {
@@ -10,4 +19,11 @@ export interface Item {
   quantity: number;
   price: number;
   link: string;
+}
+
+export interface ItemsProps {
+  items: Array<Item>;
+  handleAgregar: () => void;
+  handleEliminar: (itemIndex: number) => void;
+  handleEditar: (itemIndex: number, editedItem: Item) => void;
 }
