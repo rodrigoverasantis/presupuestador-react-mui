a137ad11ea4b8cfec019e45947ef46b4e251ae94
rodrigoverasantis <rodrigoverasantis@gmail.com>
2025-11-29 22:30:15 -0300

    feat: Actualización para mover todo el contenido a un componente container.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { Container } from "@mui/material";
 import Form from "./components/Form";
 import Items from "./components/Items";
 import Preview from "./components/Preview";
 
 export default function App() {
   return (
-    <>
+    <Container maxWidth="md">
       <Form />
       <Items />
       <Preview />
-    </>
+    </Container>
   );
 }
