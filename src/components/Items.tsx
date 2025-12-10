481337765a3d039ef308d7bac765b08d03463860
rodrigoverasantis <rodrigoverasantis@gmail.com>
2025-12-09 22:27:40 -0300

    feat: Actualización de orden de los componentes. Se incluye funcionalidad para mostrar total de todos los elementos.

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { useMemo } from "react";
 import {
   Box,
   Button,
@@ -8,28 +8,54 @@ import {
   Paper,
   TextField,
   Tooltip,
+  Typography,
 } from "@mui/material";
 import { Delete } from "@mui/icons-material";
 import type { ItemsPropsInterface } from "../interfaces";
 import { FormatoDinero } from "../utils";
 
 export default function Items(props: ItemsPropsInterface) {
+  const total = useMemo(() => props.items.reduce((prev, curr) => prev + (curr.quantity * curr.price), 0), [props.items]);
 
   return (
     <>
-      <Button
-        onClick={props.handleAgregar}
-        color="primary"
-        variant="contained"
-      >
-        Agregar elemento
-      </Button>
+      <Box display={"flex"} justifyContent={"space-between"}>
+        {/* BOTÓN PARA AGREGAR ELEMENTOS */}
+        <Button
+          onClick={props.handleAgregar}
+          color="primary"
+          variant="contained"
+        >
+          Agregar elemento
+        </Button>
+
+        {/* TOTAL DE LOS ELEMENTOS */}
+        <Chip
+          label={`Total $${FormatoDinero(total)}`}
+          variant="filled"
+          color="primary"
+          size="medium"
+          sx={{
+            fontWeight: "bold"
+          }}
+        />
+      </Box>
+
+      {/* SI NO HAY DATOS */}
+      {props.items.length === 0 && (
+        <Typography alignSelf={"center"}>
+          Sin elementos
+        </Typography>
+      )}
+
+      {/* LISTADO DE ELEMENTOS */}
       {props.items.map((item, index) => (
-        <Fragment key={`elemento_${index}`}>
-          <Paper elevation={5} sx={{ p: 2, my: 2, borderRadius: 4 }}>
-            <Grid container spacing={2}>
-              <Grid size={12}>
-                <Box display={"flex"} justifyContent={"space-between"}>
+        <Paper variant="outlined" sx={{ p: 2, my: 2 }} key={`elemento_${index}`}>
+          <Grid container spacing={2}>
+            <Grid size={12}>
+              <Box display={"flex"}>
+                <Box sx={{ flexGrow: 1 }}>
+                  {/* NÚMERO INDICADOR DEL ELEMENTO */}
                   <Chip
                     label={`Elemento #${index + 1}`}
                     color="primary"
@@ -40,69 +66,71 @@ export default function Items(props: ItemsPropsInterface) {
                     }}
                   />
 
-                  <Tooltip title="Eliminar">
-                    <IconButton onClick={() => props.handleEliminar(index)}>
+                  {/* BOTÓN PARA ELIMINAR */}
+                  <Tooltip title="Eliminar elemento" sx={{ mx: 2 }}>
+                    <IconButton
+                      onClick={() => props.handleEliminar(index)}
+                      color="default"
+                      size="small"
+                    >
                       <Delete />
                     </IconButton>
                   </Tooltip>
                 </Box>
-              </Grid>
-              <Grid size={{ xs: 12, sm: 7 }}>
-                <TextField
-                  label="Nombre"
-                  value={item.name}
-                  onChange={(event) => props.handleEditarElementos(index, { ...item, name: event.target.value })}
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
-              <Grid size={{ xs: 12, sm: 2 }}>
-                <TextField
-                  label="Cantidad"
-                  value={item.quantity}
-                  onChange={(event) => props.handleEditarElementos(index, { ...item, quantity: parseInt(event.target.value) })}
-                  type="number"
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
-              <Grid size={{ xs: 12, sm: 3 }}>
-                <TextField
-                  label="Precio"
-                  value={item.price}
-                  onChange={(event) => props.handleEditarElementos(index, { ...item, price: parseFloat(event.target.value) })}
-                  type="number"
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
-              <Grid size={12}>
-                <TextField
-                  label="Link (opcional)"
-                  value={item.link}
-                  onChange={(event) => props.handleEditarElementos(index, { ...item, link: event.target.value })}
-                  type="url"
-                  size="small"
-                  fullWidth
-                />
-              </Grid>
 
-              <Grid size={12}>
-                <Box display={"flex"} justifyContent={"end"}>
-                  <Chip
-                    label={`Subtotal $${FormatoDinero(item.quantity * item.price)}`}
-                    variant="filled"
-                    color="primary"
-                    size="small"
-                    sx={{
-                      fontWeight: "bold"
-                    }}
-                  />
-                </Box>
-              </Grid>
+                {/* SUBTOTAL DEL ELEMENTO */}
+                <Chip
+                  label={`Subtotal $${FormatoDinero(item.quantity * item.price)}`}
+                  variant="outlined"
+                  color="primary"
+                  size="medium"
+                  sx={{
+                    fontWeight: "bold"
+                  }}
+                />
+              </Box>
             </Grid>
-          </Paper>
-        </Fragment>
+            <Grid size={{ xs: 12, sm: 7 }}>
+              <TextField
+                label="Nombre"
+                value={item.name}
+                onChange={(event) => props.handleEditarElementos(index, { ...item, name: event.target.value })}
+                size="small"
+                fullWidth
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 2 }}>
+              <TextField
+                label="Cantidad"
+                value={item.quantity}
+                onChange={(event) => props.handleEditarElementos(index, { ...item, quantity: parseInt(event.target.value) })}
+                type="number"
+                size="small"
+                fullWidth
+              />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 3 }}>
+              <TextField
+                label="Precio"
+                value={item.price}
+                onChange={(event) => props.handleEditarElementos(index, { ...item, price: parseFloat(event.target.value) })}
+                type="number"
+                size="small"
+                fullWidth
+              />
+            </Grid>
+            <Grid size={12}>
+              <TextField
+                label="Link (opcional)"
+                value={item.link}
+                onChange={(event) => props.handleEditarElementos(index, { ...item, link: event.target.value })}
+                type="url"
+                size="small"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+        </Paper>
       ))}
     </>
   );
