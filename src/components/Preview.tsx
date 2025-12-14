a6b56591543515d6eaab8d19ce1daaf0367f1eb2
rodrigoverasantis <rodrigoverasantis@gmail.com>
2025-12-14 13:58:59 -0300

    feat: Actualización mayor del componente preview. Además, la tabla incluye subtotal y más.

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,6 +1,8 @@
-import type { ItemInterface, PreviewPropsInterface } from "../interfaces";
-import { FormatoFecha, FormatoDinero } from "../utils";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { useMemo } from "react";
+import { Grid, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow, Typography } from "@mui/material";
+import { Link } from "@mui/icons-material";
+import type { PreviewPropsInterface, PreviewTablePropsInterface } from "../interfaces";
+import { FormatoFecha, FormatoDinero, FormatoMoneda } from "../utils";
 
 
 export default function Preview(props: PreviewPropsInterface) {
@@ -8,51 +10,177 @@ export default function Preview(props: PreviewPropsInterface) {
   return (
     <>
       <Paper sx={{ p: 2 }}>
-        <Typography>{`Cliente: ${props.form.cliente}`}</Typography>
-        <Typography>{`Empresa: ${props.form.empresa}`}</Typography>
-        <Typography>{`RUT: ${props.form.rut}`}</Typography>
-        <Typography>{`Dirección: ${props.form.direccion}`}</Typography>
-        <Typography>{`Teléfono: ${props.form.telefono}`}</Typography>
-        <Typography>{`Email: ${props.form.email}`}</Typography>
-        <Typography>{`Ciudad: ${props.form.ciudad}`}</Typography>
-        <Typography>{`IVA: ${props.form.iva}%`}</Typography>
-        <Typography>{`Moneda: ${props.form.moneda?.label} ${props.form.moneda?.value}`}</Typography>
-        <Typography>{`Descuento: ${props.form.descuento}%`}</Typography>
-        <Typography>{`Fecha emisión: ${FormatoFecha(props.form.fechaEmision)}`}</Typography>
-        <Typography>{`Fecha vencimiento: ${FormatoFecha(props.form.fechaVencimiento)}`}</Typography>
+        <Grid container spacing={1}>
+          {/* Columna izquierda */}
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Cliente: {props.form.cliente}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Empresa: {props.form.empresa}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              RUT: {props.form.rut}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Dirección: {props.form.direccion}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Teléfono: {props.form.telefono}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Email: {props.form.email}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Ciudad: {props.form.ciudad}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              IVA: {props.form.iva}%
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Moneda: {FormatoMoneda(props.form.moneda)}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Descuento: {props.form.descuento}%
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Fecha emisión: {FormatoFecha(props.form.fechaEmision)}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Typography>
+              Fecha vencimiento: {FormatoFecha(props.form.fechaVencimiento)}
+            </Typography>
+          </Grid>
+
+          <Grid size={{ xs: 12 }}>
+            <Typography>
+              Descripción: {props.form.descripcion}
+            </Typography>
+          </Grid>
+        </Grid>
       </Paper>
 
-      <TableComponent items={props.form.items} />
+      <TableComponent
+        iva={props.form.iva}
+        descuento={props.form.descuento}
+        items={props.form.items}
+      />
     </>
   );
 }
 
-const TableComponent = (props: { items: ItemInterface[] }) => (
-  <TableContainer component={Paper} variant="outlined">
-    <Table size="small">
-      <TableHead>
-        <TableRow>
-          <TableCell>Nombre</TableCell>
-          <TableCell align="right">Cantidad</TableCell>
-          <TableCell align="right">Precio</TableCell>
-          <TableCell align="right">Subtotal</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {props.items.map((item, index) => (
-          <TableRow
-            key={`item_${index}`}
-            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {item.name}
-            </TableCell>
-            <TableCell align="right">{item.quantity}</TableCell>
-            <TableCell align="right">{item.price}</TableCell>
-            <TableCell align="right">{FormatoDinero(item.quantity * item.price)}</TableCell>
+/**
+ * Subcomponente tabla con los elementos agregados al presupusto.
+ * @param props 
+ * @returns Component.
+ */
+const TableComponent = (props: PreviewTablePropsInterface) => {
+  const montos = useMemo(() => props.items.map(i => (i.quantity * i.price) || 0), [props.items]);
+  const subtotal = useMemo(() => montos.reduce((prev, curr) => prev + curr, 0), [montos]);
+  const iva = useMemo(() => subtotal * (props.iva / 100), [subtotal, props.iva]);
+  const descuento = useMemo(() => subtotal * (props.descuento / 100), [subtotal, props.descuento]);
+  const total = useMemo(() => subtotal + iva - descuento, [subtotal, iva, descuento]);
+
+  return (
+    <TableContainer component={Paper} variant="outlined">
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Nombre</TableCell>
+            <TableCell align="right">Cantidad</TableCell>
+            <TableCell align="right">Precio</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
-);
+        </TableHead>
+
+        <TableBody>
+          {props.items.map((item, index) => (
+            <TableRow key={`item_${index}`}>
+              <TableCell component="th" scope="row">
+                {item.name}
+                {item.link && (
+                  <IconButton size="small" sx={{ ml: 1 }}>
+                    <Link />
+                  </IconButton>
+                )}
+              </TableCell>
+              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="right">{FormatoDinero(item.price)}</TableCell>
+              <TableCell align="right">{FormatoDinero(item.quantity * item.price)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3} align="right">
+              Subtotal
+            </TableCell>
+            <TableCell align="right">
+              {FormatoDinero(subtotal)}
+            </TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell colSpan={3} align="right">
+              IVA {props.iva || 0}%
+            </TableCell>
+            <TableCell align="right">
+              {FormatoDinero(iva)}
+            </TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell colSpan={3} align="right">
+              Descuento {props.descuento || 0}%
+            </TableCell>
+            <TableCell align="right">
+              {FormatoDinero(descuento)}
+            </TableCell> {/* o el valor real */}
+          </TableRow>
+
+          <TableRow>
+            <TableCell colSpan={3} align="right" sx={{ fontWeight: "bold" }}>
+              Total
+            </TableCell>
+            <TableCell align="right" sx={{ fontWeight: "bold" }}>
+              {FormatoDinero(total)}
+            </TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+}
