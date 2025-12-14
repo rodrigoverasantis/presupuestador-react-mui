c2806a9d4c0fbfba3cbe66d87f0c32299d3ee15c
rodrigoverasantis <rodrigoverasantis@gmail.com>
2025-12-14 19:05:09 -0300

    doc: Actualización para incluir documentación de componentes y funciones.

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -172,43 +172,67 @@ const TableComponent = (items: ItemInterface[], descuento: number, iva: number) 
   </TableContainer>
 );
 
-const SubtotalComponent = (items: ItemInterface[], descuento: number) => (
+/**
+ * Subcomponente con los totales para la tabla.
+ * @param items Colección de elementos.
+ * @param porcentaje Porcentaje de descuento.
+ * @returns 
+ */
+const SubtotalComponent = (items: ItemInterface[], porcentaje: number) => (
   <Box display={"flex"} justifyContent={"flex-end"}>
     <Typography
       fontSize={14}
       sx={{
-        textDecoration: descuento > 0 ? "line-through" : "inhertit",
+        textDecoration: porcentaje > 0 ? "line-through" : "inhertit",
       }}
     >
       {FormatoDinero(MontoSubtotal(items))}
     </Typography>
 
     {/* SI HAY PORCENTAJE DE DESCUENTO */}
-    {descuento > 0 && (
+    {porcentaje > 0 && (
       <Typography
         fontSize={14}
         sx={{
           ml: 1,
         }}
       >
-        {FormatoDinero(MontoDescuento(items, descuento))}
+        {FormatoDinero(MontoDescuento(items, porcentaje))}
       </Typography>
     )}
   </Box>
 );
 
+/**
+ * Método encargado de calcular el subtotal de los elementos.
+ * @param items Colección de elementos.
+ * @returns Subtotal de los elementos.
+ */
 function MontoSubtotal(items: ItemInterface[]) {
   const montos = items.map(i => (i.quantity * i.price) || 0);
   const subtotal = montos.reduce((prev, curr) => prev + curr, 0);
   return subtotal;
 }
 
+/**
+ * Método encargado de calcular el subtotal con porcentaje de decuento aplicado.
+ * @param items Colección de los elementos.
+ * @param porcentaje Porcentaje de decuento.
+ * @returns Subtotal con porcentaje de decuento aplicado.
+ */
 function MontoDescuento(items: ItemInterface[], porcentaje: number) {
   const subtotal = MontoSubtotal(items);
   const descuento = subtotal * (1 - porcentaje / 100);
   return descuento;
 }
 
+/**
+ * Método encargado de calcular el monto que corresponde al porcentaje de IVA.
+ * @param items Colección de elementos.
+ * @param porcentajeDescuento Porcentantaje de decuento.
+ * @param porcentajeIva Porcentaje de impuesto al valor agregado.
+ * @returns Monto correspondiente al IVA.
+ */
 function MontoIva(items: ItemInterface[], porcentajeDescuento: number, porcentajeIva: number) {
   const subtotal = MontoDescuento(items, porcentajeDescuento);
   const iva = subtotal * porcentajeIva / 100;
