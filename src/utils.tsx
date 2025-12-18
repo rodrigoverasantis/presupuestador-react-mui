eecf18e5fc45bb3143f8b34680670a3a528d7ea5
rodrigoverasantis <rodrigoverasantis@gmail.com>
2025-12-17 23:39:20 -0300

    feat: Se mueven funcionalidades, encargadas de calcular montos, a utilidades.

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,5 +1,41 @@
 import dayjs from "dayjs";
-import type { MonedaInterface } from "./interfaces";
+import type { ItemInterface, MonedaInterface } from "./interfaces";
+
+/**
+ * Método encargado de calcular el subtotal de los elementos.
+ * @param items Colección de elementos.
+ * @returns Subtotal de los elementos.
+ */
+export const MontoSubtotal = (items: ItemInterface[]) => {
+  const montos = items.map(i => (i.quantity * i.price) || 0);
+  const subtotal = montos.reduce((prev, curr) => prev + curr, 0);
+  return subtotal;
+}
+
+/**
+ * Método encargado de calcular el subtotal con porcentaje de decuento aplicado.
+ * @param items Colección de los elementos.
+ * @param porcentaje Porcentaje de decuento.
+ * @returns Subtotal con porcentaje de decuento aplicado.
+ */
+export const MontoDescuento = (items: ItemInterface[], porcentaje: number) => {
+  const subtotal = MontoSubtotal(items);
+  const descuento = subtotal * (1 - porcentaje / 100);
+  return descuento;
+}
+
+/**
+ * Método encargado de calcular el monto que corresponde al porcentaje de IVA.
+ * @param items Colección de elementos.
+ * @param porcentajeDescuento Porcentantaje de decuento.
+ * @param porcentajeIva Porcentaje de impuesto al valor agregado.
+ * @returns Monto correspondiente al IVA.
+ */
+export const MontoIva = (items: ItemInterface[], porcentajeDescuento: number, porcentajeIva: number) => {
+  const subtotal = MontoDescuento(items, porcentajeDescuento);
+  const iva = subtotal * porcentajeIva / 100;
+  return iva;
+}
 
 /**
  * Método encargado de formatear a formato de dinero.
